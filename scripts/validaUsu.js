422063e870eeeaf1b0bc8eb30752c18a8ac95be4
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2022-11-05 16:55:35 -0300

    Pronto para API
    Cadastro de usuário, login e produto

--- scripts/validaUsu.js
+++ scripts/validaUsu.js
@@ -5,15 +5,24 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const estado = document.getElementById("estado");
 const cidade = document.getElementById("cidade");
+const logradouro = document.getElementById("logradouro"); 
+const num = document.getElementById("num"); 
+const bairro = document.getElementById("bairro"); 
+const celular = document.getElementById("celular"); 
 const password = document.getElementById("password");
 const passwordConfirmation = document.getElementById("password-confirmation"); 
 
 var nm = document.getElementById("valNome"); 
 var em = document.getElementById("valEmail"); 
-var sn1 = document.getElementById("validaSn1"); 
-var sn2 = document.getElementById("validaSn2"); 
 var es = document.getElementById("valEstado"); 
 var cid = document.getElementById("valCidade"); 
+var lg = document.getElementById("valLog"); 
+var n = document.getElementById("valNum");
+var br = document.getElementById("valBairro"); 
+var cel = document.getElementById("valCelular"); 
+var sn1 = document.getElementById("validaSn1"); 
+var sn2 = document.getElementById("validaSn2"); 
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -24,10 +33,15 @@ form.addEventListener("submit", (e) => {
 function checkInputs() {
     const usernameValue = username.value;
     const emailValue = email.value;
-    const passwordValue = password.value;
-    const passwordConfirmationValue = passwordConfirmation.value; 
     const estadoValue = estado.value;
     const cidadeValue = cidade.value;
+    const logradouroValue = logradouro.value;
+    const numeroValue = num.value;
+    const bairroValue = bairro.value;
+    const celularValue = celular.value;
+    const passwordValue = password.value;
+    const passwordConfirmationValue = passwordConfirmation.value; 
+
     let validado = 0;
 
     if (usernameValue === "") {
@@ -60,6 +74,34 @@ function checkInputs() {
         validado++;
     }
 
+    if (logradouroValue === "") {
+        lg.className = "form-control error";
+    } else {
+        lg.className = "form-control success";
+        validado++;
+    }
+
+    if (numeroValue === "") {
+        n.className = "form-control error";
+    } else {
+        n.className = "form-control success";
+        validado++;
+    }
+
+    if (bairroValue === "") {
+        br.className = "form-control error";
+    } else {
+        br.className = "form-control success";
+        validado++;
+    }
+
+    if (usernameValue === "") {
+        cel.className = "form-control error";
+    } else {
+        cel.className = "form-control success";
+        validado++;
+    }
+
     if (passwordValue === "") {
         sn1.className = "form-control error";
     } else if (passwordValue.length < 7) {
@@ -78,7 +120,7 @@ function checkInputs() {
         validado++;
     }
 
-    if (validado === 6) {
+    if (validado === 10) {
         console.log("O formulário está 100% válido!");
     }
 }
